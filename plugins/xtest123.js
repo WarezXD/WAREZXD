89daebf3ad8cc6fb41dd596a4e53515af20ae3c1
WarezXD <admin@warezgaming.com>
2025-05-19 02:10:35 +0800

    Add files via upload

---
+++ plugins/xtest123.js
@@ -0,0 +1,9 @@
+let handler = async m => {
+
+let intro = `hello bot warez is here`
+m.reply(intro)
+}
+handler.customPrefix = /^(tes|tess|test)$/i
+handler.command = new RegExp
+
+module.exports = handler
