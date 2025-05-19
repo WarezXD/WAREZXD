db22453fc79d58570d5c1c71baa574677575ed10
WarezXD <admin@warezgaming.com>
2025-05-19 15:32:52 +0800

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -23,7 +23,7 @@ handler.help = ['kick @user']
 handler.tags = ['group']
 handler.command = /^(kic?k|remove|tendang|\-)$/i
 
+handler.admin = true
 handler.group = true
-handler.botAdmin = true
 
 module.exports = handler
